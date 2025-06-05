31dab05690fe549179030a4619ab987c90604593
Tarun25-dev <22x51a3225@srecnandyal.edu.in>
2025-06-05 21:45:31 +0530

    Update 6de.js

--- 6de.js
+++ 6de.js
@@ -1,10 +1,10 @@
-6d.Imagine an amusement park that has a discount for children (6 years and younger) or seniors (65 years and older).
-Create a variable 'age' and save a person's age inside.
-Create an if-statement that checks if the person qualifies for a discount. If they do, display 'Discount' in the console. Otherwise, display 'No discount' in the console.
-Note: try to use the || operator in your solution.
-Try changing the 'age' variable to display different messages.
-6e.Continuing from excercise,
-let's say the discount is only available if it is not a holiday. Create a variable: const isHoliday = true;
-Update the code so that in order to get a discount, people must meet the age requirement and it is also not a holiday.
-Note: && has a higher priority than || so you may need to use brackets () to control which code gets done first.
-Try changing the value of isHoliday to display different messages.
+let age=prompt('Enter age :');
+console.log('today is holiday type y otherwise n : ');
+let isHoliday=prompt();
+if(isHoliday==="y")
+  if(age<=6 || age>=60){
+    console.log('discount is applicable');
+}
+else{
+    console.log('no discount');
+}
